91f30d3f82bf221d8b9f51dffb4ac8b8f6f5caf7
Nicolai Parlog <nicolai@nipafx.dev>
2024-04-13 23:02:34 +0200

    Update Ginevra

--- ginevra/src/main/java/dev/nipafx/ginevra/Ginevra.java
+++ ginevra/src/main/java/dev/nipafx/ginevra/Ginevra.java
@@ -2,6 +2,8 @@ package dev.nipafx.ginevra;
 
 import dev.nipafx.args.Args;
 import dev.nipafx.args.ArgsParseException;
+import dev.nipafx.args.Parsed2;
+import dev.nipafx.args.Parsed3;
 import dev.nipafx.ginevra.execution.FullOutliner;
 import dev.nipafx.ginevra.execution.Paths;
 import dev.nipafx.ginevra.execution.Store;
@@ -38,11 +40,8 @@ public class Ginevra {
 	}
 
 	public static Ginevra initialize(String[] args, UnaryOperator<Configuration> updateConfig) {
-		var store = new Store();
 		var paths = parseConfiguration(args, updateConfig).createPaths();
-		var markdownParser = locateMarkdownParser();
-		var renderer = new Renderer(store, paths.resourcesFolder(), paths.cssFolder());
-		return new Ginevra(store, markdownParser, renderer, paths);
+		return createGinevra(paths);
 	}
 
 	private static Configuration parseConfiguration(String[] args, UnaryOperator<Configuration> updateConfig) {
@@ -51,6 +50,56 @@ public class Ginevra {
 		} catch (ArgsParseException ex) {
 			throw new IllegalArgumentException(ex);
 		}
+	}
+
+	public static <CONFIG extends Record> GinevraWithArgs<CONFIG> initialize(String[] args, Class<CONFIG> configType) {
+		return initialize(args, configType, identity());
+	}
+
+	public static <CONFIG extends Record> GinevraWithArgs<CONFIG> initialize(
+			String[] args, Class<CONFIG> configType, UnaryOperator<Configuration> updateConfig) {
+		var configuration = parseConfiguration(args, configType, updateConfig);
+		var ginevra = createGinevra(configuration.first().createPaths());
+		return new GinevraWithArgs<>(ginevra, configuration.second());
+	}
+
+	private static <CONFIG extends Record> Parsed2<Configuration, CONFIG> parseConfiguration(
+			String[] args, Class<CONFIG> configType, UnaryOperator<Configuration> updateConfig) {
+		try {
+			var parsed = Args.parse(args, Configuration.class, configType);
+			return new Parsed2<>(updateConfig.apply(parsed.first()), parsed.second());
+		} catch (ArgsParseException ex) {
+			throw new IllegalArgumentException(ex);
+		}
+	}
+
+	public static <CONFIG1 extends Record, CONFIG2 extends Record> GinevraWith2Args<CONFIG1, CONFIG2> initialize(
+			String[] args, Class<CONFIG1> configType1, Class<CONFIG2> configType2) {
+		return initialize(args, configType1, configType2, identity());
+	}
+
+	public static <CONFIG1 extends Record, CONFIG2 extends Record> GinevraWith2Args<CONFIG1, CONFIG2> initialize(
+			String[] args, Class<CONFIG1> configType1, Class<CONFIG2> configType2, UnaryOperator<Configuration> updateConfig) {
+		var configuration = parseConfiguration(args, configType1, configType2, updateConfig);
+		var ginevra = createGinevra(configuration.first().createPaths());
+		return new GinevraWith2Args<>(ginevra, configuration.second(), configuration.third());
+	}
+
+	private static <CONFIG1 extends Record, CONFIG2 extends Record> Parsed3<Configuration, CONFIG1, CONFIG2> parseConfiguration(
+			String[] args, Class<CONFIG1> configType1, Class<CONFIG2> configType2, UnaryOperator<Configuration> updateConfig) {
+		try {
+			var parsed = Args.parse(args, Configuration.class, configType1, configType2);
+			return new Parsed3<>(updateConfig.apply(parsed.first()), parsed.second(), parsed.third());
+		} catch (ArgsParseException ex) {
+			throw new IllegalArgumentException(ex);
+		}
+	}
+
+	private static Ginevra createGinevra(Paths paths) {
+		var store = new Store();
+		var markdownParser = locateMarkdownParser();
+		var renderer = new Renderer(store, paths.resourcesFolder(), paths.cssFolder());
+		return new Ginevra(store, markdownParser, renderer, paths);
 	}
 
 	private static Optional<MarkdownParser> locateMarkdownParser() {
@@ -95,5 +144,9 @@ public class Ginevra {
 		}
 
 	}
+
+	public record GinevraWithArgs<CONFIG extends Record>(Ginevra ginevra, CONFIG config) { }
+
+	public record GinevraWith2Args<CONFIG1 extends Record, CONFIG2 extends Record>(Ginevra ginevra, CONFIG1 config1, CONFIG2 config2) { }
 
 }
